4a05c46536ff7555f6a4827505bf6f5fc5ad2e41
Dylan Liew <Dylanliew0503@gmail.com>
2025-01-16 18:35:41 +0800

    Updated Forgot Password

---
+++ src/pages/ForgotPassword.tsx
@@ -0,0 +1,70 @@
+export default function ForgotPassword() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1 h-screen">
+        <div className="flex flex-1 flex-col px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="flex mt-5 h-full flex-col justify-center px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                Forgot Password
+              </h2>
+              <p className="text-center text-sm/6 text-gray-500 hover:text-gray-700">
+                No worries, we'll send you the reset instructions.
+              </p>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form action="#" method="POST" className="space-y-6">
+                <div>
+                  <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                    Phone Number
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      required
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="mt-5 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Reset Password
+                  </button>
+                </div>
+              </form>
+              <p className="mt-10 text-center text-sm/6 text-gray-500 hover:text-gray-700">
+                <a href="/login" className="flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-4 w-4 mr-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                    />
+                  </svg>
+                  Back to Login
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            alt=""
+            src="/src/img/mwh2.jpeg?auto=format&fit=crop&w=1908&q=80"
+            className="absolute inset-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+
