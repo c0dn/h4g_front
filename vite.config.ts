1cdb8faeb78f71f67b844e86793e44e10418b730
c0dn <19631981+c0dn@users.noreply.github.com>
2025-01-17 23:35:43 +0800

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ _command, mode }) => {
+export default defineConfig(({ mode }) => {
   // Load env file based on `mode`
   const env = loadEnv(mode, process.cwd(), '')
 
